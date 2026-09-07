d5766dc2ef6e5cdc03bc51e7256cb19636f2f8d4
Vishwas <16206966+bevishwas@users.noreply.github.com>
2026-09-07 18:42:05 +0530

    bug fix

--- src/menu-items/platform.jsx
+++ src/menu-items/platform.jsx
@@ -174,7 +174,7 @@ const platformMenuGroups = [
     title: 'marketing',
     children: [
       { id: 'banners', title: 'banners', type: 'item', url: '/banners', icon: icons.PictureOutlined, perm: 'banner', action: 'read' },
-      // { id: 'share-links', title: 'share-links', type: 'item', url: '/share-links', icon: icons.ShareAltOutlined, perm: 'shareLink', action: 'read' },
+      { id: 'share-links', title: 'share-links', type: 'item', url: '/share-links', icon: icons.ShareAltOutlined, perm: 'shareLink', action: 'read' },
       { id: 'promotion-campaigns', title: 'campaigns', type: 'item', url: '/promotion-campaigns', icon: icons.NotificationOutlined, perm: 'promotion', action: 'read' },
       { id: 'promotions', title: 'promotions', type: 'item', url: '/promotions', icon: icons.GiftOutlined, perm: 'promotion', action: 'read' },
       { id: 'faqs', title: 'faqs', type: 'item', url: '/faqs', icon: icons.QuestionCircleOutlined, perm: 'faq', action: 'read' }
