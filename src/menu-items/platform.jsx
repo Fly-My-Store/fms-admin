a7903eadec2e54006af55e006099cf33ebf333bc
Vishwas <16206966+bevishwas@users.noreply.github.com>
2026-09-18 11:58:16 +0530

    bug fix

--- src/menu-items/platform.jsx
+++ src/menu-items/platform.jsx
@@ -182,7 +182,7 @@ const platformMenuGroups = [
       { id: 'share-links', title: 'share-links', type: 'item', url: '/share-links', icon: icons.ShareAltOutlined, perm: 'shareLink', action: 'read' },
       { id: 'promotion-campaigns', title: 'campaigns', type: 'item', url: '/promotion-campaigns', icon: icons.NotificationOutlined, perm: 'promotion', action: 'read' },
       { id: 'promotions', title: 'promotions', type: 'item', url: '/promotions', icon: icons.GiftOutlined, perm: 'promotion', action: 'read' },
-      { id: 'surges', title: 'surges', type: 'item', url: '/surges', icon: icons.ThunderboltOutlined, perm: 'promotion', action: 'read' },
+      // { id: 'surges', title: 'surges', type: 'item', url: '/surges', icon: icons.ThunderboltOutlined, perm: 'promotion', action: 'read' },
       { id: 'faqs', title: 'faqs', type: 'item', url: '/faqs', icon: icons.QuestionCircleOutlined, perm: 'faq', action: 'read' }
     ]
   },
@@ -218,15 +218,15 @@ const platformMenuGroups = [
         url: '/app-releases',
         icon: icons.MobileOutlined
       },
-      {
-        id: 'app-updates',
-        title: 'app-updates',
-        type: 'item',
-        url: '/app-updates',
-        icon: icons.CloudSyncOutlined,
-        perm: 'appRelease',
-        action: 'read'
-      }
+      // {
+      //   id: 'app-updates',
+      //   title: 'app-updates',
+      //   type: 'item',
+      //   url: '/app-updates',
+      //   icon: icons.CloudSyncOutlined,
+      //   perm: 'appRelease',
+      //   action: 'read'
+      // }
     ]
   },
   {
